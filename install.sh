75753682805670566d04a2883db9550d4deda348
Andy Shand <andy@morethanyouknow.co.uk>
2022-06-05 15:07:22 +0100

    updates

--- install.sh
+++ install.sh
@@ -4,6 +4,5 @@ sudo apt-get install git ffmpeg libsm6 libxext6 -y
 rm -rf third-eye-api
 git clone --depth 1 "https://github.com/andyshand/third-eye-api.git"
 
+source /root/venv/activate
 cd third-eye-api
-python3 -m venv ./venv
-source ./venv/bin/activate
--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 
 import os
 
+from DefaultPaths import DefaultPaths
+
 
 def cmd(cmd):
     os.system(cmd)
@@ -40,51 +42,48 @@ version_str = "".join([
 clone_repo("https://github.com/MSFTserver/pytorch3d-lite.git")
 sys.path.append('./pytorch3d-lite')
 
-root_path = f'.'
-model_path = root_path
+pathlib.Path(DefaultPaths.model_path).mkdir(parents=True, exist_ok=True)
 
-pathlib.Path(model_path).mkdir(parents=True, exist_ok=True)
-
-if not (path_exists(f'{model_path}/512x512_diffusion_uncond_finetune_008100.pt')):
+if not (path_exists(f'{DefaultPaths.model_path}/512x512_diffusion_uncond_finetune_008100.pt')):
     wget.download(
-        "https://the-eye.eu/public/AI/models/512x512_diffusion_unconditional_ImageNet/512x512_diffusion_uncond_finetune_008100.pt", model_path)
-if not (path_exists(f'{model_path}/secondary_model_imagenet_2.pth')):
+        "https://the-eye.eu/public/AI/models/512x512_diffusion_unconditional_ImageNet/512x512_diffusion_uncond_finetune_008100.pt", DefaultPaths.model_path)
+if not (path_exists(f'{DefaultPaths.model_path}/secondary_model_imagenet_2.pth')):
     wget.download(
-        "https://the-eye.eu/public/AI/models/v-diffusion/secondary_model_imagenet_2.pth", model_path)
-if not (path_exists(f'{model_path}/AdaBins_nyu.pt')):
+        "https://the-eye.eu/public/AI/models/v-diffusion/secondary_model_imagenet_2.pth", DefaultPaths.model_path)
+if not (path_exists(f'{DefaultPaths.model_path}/AdaBins_nyu.pt')):
     wget.download(
-        "https://cloudflare-ipfs.com/ipfs/Qmd2mMnDLWePKmgfS8m6ntAg4nhV5VkUyAydYBp8cWWeB7/AdaBins_nyu.pt", model_path)
-if not (path_exists(f'{model_path}/vqgan_imagenet_f16_16384.ckpt')):
+        "https://cloudflare-ipfs.com/ipfs/Qmd2mMnDLWePKmgfS8m6ntAg4nhV5VkUyAydYBp8cWWeB7/AdaBins_nyu.pt", DefaultPaths.model_path)
+if not (path_exists(f'{DefaultPaths.model_path}/vqgan_imagenet_f16_16384.ckpt')):
     # ImageNet 16384
-    cmd(f'curl -L -o "{model_path}/vqgan_imagenet_f16_16384.yaml" -C - "https://heibox.uni-heidelberg.de/d/a7530b09fed84f80a887/files/?p=%2Fconfigs%2Fmodel.yaml&dl=1"')
+    cmd(f'curl -L -o "{DefaultPaths.model_path}/vqgan_imagenet_f16_16384.yaml" -C - "https://heibox.uni-heidelberg.de/d/a7530b09fed84f80a887/files/?p=%2Fconfigs%2Fmodel.yaml&dl=1"')
     # ImageNet 16384
-    cmd(f'curl -L -o "{model_path}/vqgan_imagenet_f16_16384.ckpt" -C - "https://heibox.uni-heidelberg.de/d/a7530b09fed84f80a887/files/?p=%2Fckpts%2Flast.ckpt&dl=1"')
-if not (path_exists(f'{model_path}/diffusion.pt')):
-    cmd(f'wget -c - O "{model_path}/diffusion.pt" "https://dall-3.com/models/glid-3-xl/diffusion.pt"')
-if not (path_exists(f'{model_path}/finetune.pt')):
+    cmd(f'curl -L -o "{DefaultPaths.model_path}/vqgan_imagenet_f16_16384.ckpt" -C - "https://heibox.uni-heidelberg.de/d/a7530b09fed84f80a887/files/?p=%2Fckpts%2Flast.ckpt&dl=1"')
+if not (path_exists(f'{DefaultPaths.model_path}/diffusion.pt')):
+    cmd(f'wget -c - O "{DefaultPaths.model_path}/diffusion.pt" "https://dall-3.com/models/glid-3-xl/diffusion.pt"')
+if not (path_exists(f'{DefaultPaths.model_path}/finetune.pt')):
     cmd(
-        f'wget -c "https://dall-3.com/models/glid-3-xl/finetune.pt" -O "{model_path}/finetune.pt"')
-if not (path_exists(f'{model_path}/bert.pt')):
+        f'wget -c "https://dall-3.com/models/glid-3-xl/finetune.pt" -O "{DefaultPaths.model_path}/finetune.pt"')
+if not (path_exists(f'{DefaultPaths.model_path}/bert.pt')):
     cmd(
-        f'wget -c "https://dall-3.com/models/glid-3-xl/bert.pt" -O "{model_path}/bert.pt"')
-if not (path_exists(f'{model_path}/kl-f8.pt')):
+        f'wget -c "https://dall-3.com/models/glid-3-xl/bert.pt" -O "{DefaultPaths.model_path}/bert.pt"')
+if not (path_exists(f'{DefaultPaths.model_path}/kl-f8.pt')):
     cmd(
-        f'wget -c https://dall-3.com/models/glid-3-xl/kl-f8.pt -O "{model_path}/kl-f8.pt"')
+        f'wget -c https://dall-3.com/models/glid-3-xl/kl-f8.pt -O "{DefaultPaths.model_path}/kl-f8.pt"')
 
 clone_repo("https://github.com/CompVis/taming-transformers.git")
 clone_repo("https://github.com/openai/CLIP.git")
 clone_repo("https://github.com/crowsonkb/guided-diffusion.git")
 clone_repo("https://github.com/assafshocher/ResizeRight.git")
 clone_repo("https://github.com/isl-org/MiDaS.git")
-if not path_exists(f'{root_path}/MiDaS/midas_utils.py'):
+if not path_exists(f'{DefaultPaths.root_path}/MiDaS/midas_utils.py'):
     os.rename("MiDaS/utils.py", "MiDaS/midas_utils.py")
 clone_repo("https://github.com/CompVis/latent-diffusion.git")
 clone_repo("https://github.com/shariqfarooq123/AdaBins.git")
 clone_repo("https://github.com/alembics/disco-diffusion.git")
 clone_repo("https://github.com/Jack000/glid-3-xl")
-if not path_exists(f'{root_path}/glid-3-xl/jack_guided_diffusion'):
+if not path_exists(f'{DefaultPaths.root_path}/glid-3-xl/jack_guided_diffusion'):
     os.rename('glid-3-xl/guided_diffusion', 'glid-3-xl/jack_guided_diffusion')
 
-if not path_exists(f'{root_path}/disco_xform_utils.py'):
+if not path_exists(f'{DefaultPaths.root_path}/disco_xform_utils.py'):
     shutil.copyfile("disco-diffusion/disco_xform_utils.py",
                     "disco_xform_utils.py")
