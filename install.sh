3695ec956438f87e7157521b7bd49c49c342ae52
Andy Shand <andy@morethanyouknow.co.uk>
2022-06-05 14:58:50 +0100

    updates

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 sudo apt-get install git ffmpeg libsm6 libxext6 -y
+rm -rf third-eye-api
 git clone --depth 1 "https://github.com/andyshand/third-eye-api.git"
+
+cd third-eye-api
 python3 -m venv ./venv
 source ./venv/bin/activate
