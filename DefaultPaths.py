599333a8bbe8d8e61ace7cf9c2769441ea74ade7
Andy Shand <andy@morethanyouknow.co.uk>
2022-06-05 16:47:57 +0100

    push

--- DefaultPaths.py
+++ DefaultPaths.py
@@ -1,9 +1,10 @@
 
+import os
 from os.path import exists as path_exists
-
+home_path = os.environ.get('HOME')
 
 class DefaultPaths:
     root_path = f"."
     is_drive = False
-    model_path = root_path
+    model_path = f"{home_path}/models"
     output_path = f"{root_path}/outputs"
