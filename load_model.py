c3312c21b7a64d0b9758237f4ee9a7e7d88ac91e
Andy Shand <andy@morethanyouknow.co.uk>
2022-06-10 20:25:40 +0100

    changes

--- load_model.py
+++ load_model.py
@@ -43,9 +43,8 @@ def load_gaussian_diffusion_model_2(diffusion_model, model_config, device, gener
             param.requires_grad_()
     if model_config["use_fp16"]:
         model.convert_to_fp16()
-    return create_gaussian_diffusion(**model_config)
 
-  diffusion = load_or_cached("gaussian_diffusion_2", create, generation)
+  model, diffusion = load_or_cached("gaussian_diffusion_2", create, generation)
 
 def load_secondary_model(device, generation):
   def create():
